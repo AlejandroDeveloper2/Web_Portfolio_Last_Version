0a272c7ecce40421d2b59f6d8841dfacc52de975
AlejandroDeveloper2 <90485793+AlejandroDeveloper2@users.noreply.github.com>
2025-02-07 13:45:22 -0500

    Actualización portfolio, lazy loading de componente modal

--- src/components/my-projects/project-card-list/ProjectCardList.tsx
+++ src/components/my-projects/project-card-list/ProjectCardList.tsx
@@ -1,11 +1,15 @@
+import { lazy, Suspense } from "react";
+
 import { ProjectCardListProps } from "@interfaces/component-types";
 import { Project } from "@interfaces/data-types";
 
 import useModal from "@hooks/useModal";
 
-import { LoadingBar, Modal, ProjectCard } from "@components/index";
+import { LoadingBar, ProjectCard } from "@components/index";
 
 import styles from "./ProjectCardList.module.css";
+
+const Modal = lazy(() => import("@components/my-projects/modal/Modal"));
 
 const ProjectCardList = ({
   projects,
@@ -15,12 +19,14 @@ const ProjectCardList = ({
 
   return (
     <>
-      {modalData ? (
-        <Modal
-          projectData={modalData}
-          isModalVisible={isModalVisible}
-          toggleModal={toggleModal}
-        />
+      {modalData && isModalVisible ? (
+        <Suspense fallback={<></>}>
+          <Modal
+            projectData={modalData}
+            isModalVisible={isModalVisible}
+            toggleModal={toggleModal}
+          />
+        </Suspense>
       ) : null}
       <ul className={styles.projectCardList}>
         {projectsLoading ? (
